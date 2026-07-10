a38367cd0c14d7345fed5f546a80216db1848430
asukhoy <asuhomlin2007@yandex.ru>
2026-07-10 21:21:36 +0300

    Init commit

---
+++ src/test/java/TaskFlow/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package TaskFlow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
